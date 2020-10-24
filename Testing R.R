2acaaf54975da5730397b0765be3074cb3f14d2a
Aldo <disoan1@hotmail.com>
2020-10-24 16:03:28 -0300

    2da opcio

--- Testing R.R
+++ Testing R.R
@@ -2,4 +2,6 @@ print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
 
-print Aldo Torrecilla
+print ("Aldo TORRECILLA Castro")
+
+print ("Tengo que agregar esta linea para hacer un nuevo actualizacion")
