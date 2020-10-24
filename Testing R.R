3d5ac231f518ec23d4c6d8f55d91c1930e66f6d8
Aldo <disoan1@hotmail.com>
2020-10-24 16:09:50 -0300

    3ra correccion

--- Testing R.R
+++ Testing R.R
@@ -5,3 +5,10 @@ print("And now it lives on GitHub")
 print ("Aldo TORRECILLA Castro")
 
 print ("Tengo que agregar esta linea para hacer un nuevo actualizacion")
+
+
+Print ("tengo una gran pena por mi hijo Diego, Dioste pido por su trabajo")
+
+Print ("por sofia y Andrea tengo felicidad porque estan en camino de su vida")
+
+print ("Señor mio te pido por mis tres hijos de guiarlos por tu camino")
